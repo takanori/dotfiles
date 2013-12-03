776b8b07139b9a8fd84dd48d1f9bf6bb4dc31743
takanori <telespectroscope@gmail.com>
2013-12-03 20:08:55 +0900

    Add z.sh setting in .zshrc

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ fi
 [ ! -d ~/.oh-my-zsh ] && curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 rm -f ~/.zshrc
 
-DOT_FILES=( .ctags .gvimrc .perltidyrc .tmux.conf .vimrc .zshrc .zshrc.alias .zshrc.custom .zshrc.osx .zshrc.linux )
+DOT_FILES=( .ctags .gvimrc .perltidyrc .profile .tmux.conf .vimrc .zshrc .zshrc.alias .zshrc.custom .zshrc.osx .zshrc.linux )
 
 for file in ${DOT_FILES[@]}
 do
