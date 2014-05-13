5baa4eae01fe4b2e31a9cc8e890b179e5c13d325
takanori <takanori.uzuka@dena.com>
2014-05-13 17:12:41 +0900

    Fix setup.sh.

--- setup.sh
+++ setup.sh
@@ -40,11 +40,11 @@ DOT_FILES_TO_COPY=( .gitconfig )
 
 for file_to_copy in ${DOT_FILES_TO_COPY[@]}
 do
-	if [ -f $HOME/$file ] ; then
-		printf "%-30s already exists.\n" $file 
+	if [ -f $HOME/$file_to_copy ] ; then
+		printf "%-30s already exists.\n" $file_to_copy
 	else
-		cp $HOME/dotfiles/$file $HOME/$file
-		printf "Made copy $HOME/$file\n" 
+		cp $HOME/dotfiles/$file_to_copy $HOME/$file_to_copy
+		printf "Made copy $HOME/$file_to_copy\n" 
 	fi
 done
 
